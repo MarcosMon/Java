dfe5bac423601758ac355e64fdfb5a0ec8f1460e
marc monjon <sysmarc13@hotmail.com>
2019-01-30 05:01:23 +0100

    Kata al parecer terminado sin refactorizar

--- bowlingGame/src/main/java/org/lasencinas/bowlingGame/ScoreCard.java
+++ bowlingGame/src/main/java/org/lasencinas/bowlingGame/ScoreCard.java
@@ -1,55 +1,74 @@
 package org.lasencinas.bowlingGame;
 
 public class ScoreCard {
-	
-	private String tiradas = "";
-	private int puntuacionTotal=0;
-	public static String symbols = "-123456789X/";
-	
-	public ScoreCard() {
-		
-	}
-	public ScoreCard(String tiradas) {
-		this.tiradas = tiradas;
-	}
-	
-	public String getTiradas() {
-		return this.tiradas;
-	}
-	
-	public int getPuntuacionTotal() {
-		return this.puntuacionTotal;
-	}
-	
-	public void setPuntuacion(int puntuacion) {
-		this.puntuacionTotal=puntuacion;
-	}
-	
-	public void calcularPuntuacion() {
-		int puntuacion = 0;
-		
-		for (int i = 0; i < getTiradas().length(); i++) {
-			 
-				if (getTiradas().charAt(i) == 'X') {
-					int segundaTirada = symbols.indexOf(getTiradas().charAt(i+2));
-					puntuacion+=10 +  symbols.indexOf(getTiradas().charAt(i+1) + segundaTirada);
-				}
-				else if(getTiradas().charAt(i) == '/') {
-					int tiradaAnterior = symbols.indexOf(getTiradas().charAt(i-1));
-					if (i < 19) {
-						puntuacion+=10 + symbols.indexOf(getTiradas().charAt(i+1)) - tiradaAnterior;
-				}
-					else {
-						puntuacion+=10  - tiradaAnterior;
-					}
-				}
-				else {
-					puntuacion+=symbols.indexOf(getTiradas().charAt(i));
-				}
-				
-		}
-		setPuntuacion(puntuacion);
-	}
-		
 
-	}	
+    private String tiradas = "";
+    private int puntuacionTotal = 0;
+    public static String symbols = "-123456789X/";
+
+    public ScoreCard() {
+
+    }
+
+    public ScoreCard(String tiradas) {
+	this.tiradas = tiradas;
+    }
+
+    public String getTiradas() {
+	return this.tiradas;
+    }
+
+    public int getPuntuacionTotal() {
+	return this.puntuacionTotal;
+    }
+
+    public void setPuntuacion(int puntuacion) {
+	this.puntuacionTotal = puntuacion;
+    }
+
+    public void calcularPuntuacion() {
+	int puntuacion = 0;
+	int contar = 0;
+	for (int i = 0; i < getTiradas().length(); i++) {
+
+	    if (getTiradas().charAt(i) == 'X') {
+		contar++;
+		if (contar < 19) {
+		    if (getTiradas().charAt(i + 1) == 'X') {
+			int primeraTirada = symbols.indexOf(getTiradas().charAt(i + 1) * 2);
+			int segundaTirada = symbols.indexOf(getTiradas().charAt(i + 2));
+			puntuacion += 21 + primeraTirada + segundaTirada;
+		    }
+
+		    else if (getTiradas().charAt(i + 2) == '/') {
+			puntuacion += 20;
+		    } else {
+			int primeraTirada = symbols.indexOf(getTiradas().charAt(i + 1));
+			int segundaTirada = symbols.indexOf(getTiradas().charAt(i + 2));
+
+			puntuacion += 10 + primeraTirada + segundaTirada;
+		    }
+
+		} else {
+		    puntuacion += 10;
+		}
+
+	    }
+
+	    else if (getTiradas().charAt(i) == '/') {
+		int tiradaAnterior = symbols.indexOf(getTiradas().charAt(i - 1));
+		if (contar < 19) {
+		    puntuacion += 10 + symbols.indexOf(getTiradas().charAt(i + 1)) - tiradaAnterior;
+		} else {
+		    puntuacion += 10 - tiradaAnterior;
+		}
+	    } else {
+		puntuacion += symbols.indexOf(getTiradas().charAt(i));
+	    }
+	    contar += 1;
+
+	}
+	setPuntuacion(puntuacion);
+    }
+
+}
--- bowlingGame/src/test/java/org/lasencinas/bowlingGame/ScoreCardTest.java
+++ bowlingGame/src/test/java/org/lasencinas/bowlingGame/ScoreCardTest.java
@@ -57,5 +57,101 @@ public class ScoreCardTest {
 		int total = 92;
 		assertEquals(total, card.getPuntuacionTotal());
 	} 
+	@Test
+	public void Test5() {
+		String tiradas = "9-3/613/815/-/8-7/8/8";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 131;
+		assertEquals(total, card.getPuntuacionTotal());
+	} 
+	@Test
+	public void test6() {
+		String tiradas = "9-3561368153258-7181";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 82;
+		assertEquals(total, card.getPuntuacionTotal());
+	}
+	@Test
+	public void test7() {
+		String tiradas = "X9-9-9-9-9-9-9-9-9-";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 100;
+		assertEquals(total, card.getPuntuacionTotal());
+	} 
+	@Test
+	public void test8() {
+		String tiradas = "9-9-9-9-9-9-9-9-9-X9-";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 100;
+		assertEquals(total, card.getPuntuacionTotal());
+	} 
+	@Test
+	public void test9() {
+		String tiradas = "X9-X9-9-9-9-9-9-9-";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 110;
+		assertEquals(total, card.getPuntuacionTotal());
+	} 
+	@Test
+	public void test10() {
+		String tiradas = "XX9-9-9-9-9-9-9-9-";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 120;
+		assertEquals(total, card.getPuntuacionTotal());
+	}
+	@Test
+	public void test11() {
+		String tiradas = "XXX9-9-9-9-9-9-9-";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 141;
+		assertEquals(total, card.getPuntuacionTotal());
+	} 
+	@Test
+	public void test12() {
+		String tiradas = "9-9-9-9-9-9-9-9-9-XXX";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 111;
+		assertEquals(total, card.getPuntuacionTotal());
+	} 
+	@Test
+	public void test13() {
+		String tiradas = "X5/X5/XX5/--";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 135;
+		assertEquals(total, card.getPuntuacionTotal());
+	} 
+	@Test
+	public void test14() {
+		String tiradas = "X5/X5/XX5/--5/X5/";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 175;
+		assertEquals(total, card.getPuntuacionTotal());
+	}
+	@Test
+	public void test15() {
+		String tiradas = "8/549-XX5/53639/9/X";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 149;
+		assertEquals(total, card.getPuntuacionTotal());
+	}
+	@Test
+	public void test16() {
+		String tiradas = "XXXXXXXXXXXX";
+		card = new ScoreCard(tiradas);
+		card.calcularPuntuacion();
+		int total = 300;
+		assertEquals(total, card.getPuntuacionTotal());
+	}
 }
 
