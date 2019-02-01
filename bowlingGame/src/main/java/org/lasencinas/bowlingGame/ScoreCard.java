85e958e7d3e10dae1f01344fb4c1cee2dafd4b46
marc monjon <sysmarc13@hotmail.com>
2019-02-01 04:34:58 +0100

    Refactorizacion terminada y pasa todos los casos test

--- bowlingGame/src/main/java/org/lasencinas/bowlingGame/ScoreCard.java
+++ bowlingGame/src/main/java/org/lasencinas/bowlingGame/ScoreCard.java
@@ -6,16 +6,22 @@ public class ScoreCard {
     private int puntuacionTotal = 0;
     private int STRIKE = 10;
     private int SPARE = 10;
-    public static String symbols = "-123456789X/";
-    
+    private int posicionUno = 1;
+    private int posicionDos = 2;
 
-    public ScoreCard() {
+    public static String symbols = "-123456789X/";
+
+    // ------------Constructores------------------//
+
+    ScoreCard() {
 
     }
 
     public ScoreCard(String tiradas) {
 	this.tiradas = tiradas;
     }
+    
+    /*---------------- Getters and Setters -------------------------*/
 
     public String getTiradas() {
 	return this.tiradas;
@@ -28,50 +34,71 @@ public class ScoreCard {
     public void setPuntuacion(int puntuacion) {
 	this.puntuacionTotal = puntuacion;
     }
+    
+    /*---------------- Métodos -------------------------*/
+
+    public int longitudCarton() {
+	return getTiradas().length();
+    }
+
+    public int bolosDerribados(char bolos) {
+	return symbols.indexOf(bolos);
+    }
+
+    public int recuentoLanzamientos(int tirada) {
+	return bolosDerribados(getTiradas().charAt(tirada));
+
+    }
+
+    public int derribo(int bola) {
+	return getTiradas().charAt(bola);
+    }
 
     public void calcularPuntuacion() {
 	int puntuacion = 0;
 	int posicionesTarjeta = 0;
-	for (int i = 0; i < getTiradas().length(); i++) {
-	    if (getTiradas().charAt(i) == 'X') {
+
+	for (int bola = 0; bola < longitudCarton(); bola++) {
+
+	    if (derribo(bola) == 'X') {
 		posicionesTarjeta++;
 		if (posicionesTarjeta < 19) {
-		    if (getTiradas().charAt(i + 1) == 'X' || getTiradas().charAt(i+2) == 'X') {
-			int primeraTirada = symbols.indexOf(getTiradas().charAt(i + 1)*2);
-			int segundaTirada = symbols.indexOf(getTiradas().charAt(i + 2));
-			puntuacion += 21 + primeraTirada + segundaTirada;
+		    if (recuentoLanzamientos(bola + posicionUno) == 'X') {
+
+			puntuacion += STRIKE + recuentoLanzamientos(bola + posicionUno)
+				+ recuentoLanzamientos(bola + posicionDos);
 		    }
 
-		    else if (getTiradas().charAt(i + 2) == '/') {
+		    else if (derribo(bola + posicionDos) == '/') {
 			puntuacion += STRIKE + SPARE;
 		    } else {
-			int primeraTirada = symbols.indexOf(getTiradas().charAt(i + 1));
-			int segundaTirada = symbols.indexOf(getTiradas().charAt(i + 2));
 
-			puntuacion += STRIKE + primeraTirada + segundaTirada;
+			puntuacion += STRIKE + recuentoLanzamientos(bola + posicionUno)
+				+ recuentoLanzamientos(bola + posicionDos);
 		    }
 
 		} else {
 		    puntuacion += STRIKE;
 		}
 
-	    }
-
-	    else if (getTiradas().charAt(i) == '/') {
-		int tiradaAnterior = symbols.indexOf(getTiradas().charAt(i - 1));
-		
+	    } else if (derribo(bola) == '/') {
 		if (posicionesTarjeta < 19) {
-		    puntuacion += SPARE + symbols.indexOf(getTiradas().charAt(i + 1)) - tiradaAnterior;
-	        }else {
-		    puntuacion += SPARE - tiradaAnterior;
+
+		    puntuacion += SPARE + recuentoLanzamientos(bola + posicionUno) - recuentoLanzamientos(bola - 1);
+		} else {
+		    puntuacion += SPARE - recuentoLanzamientos(bola - 1);
 		}
 	    } else {
-		puntuacion += symbols.indexOf(getTiradas().charAt(i));
+		puntuacion += recuentoLanzamientos(bola);
 	    }
-	    posicionesTarjeta += 1;
 
+	    posicionesTarjeta++;
 	}
 	setPuntuacion(puntuacion);
+    }
+
+    public void mostrarPuntuacionTotal() {
+	System.out.println("Tu puntuación total es de: " + getPuntuacionTotal() + " Puntos");
     }
 
 }
