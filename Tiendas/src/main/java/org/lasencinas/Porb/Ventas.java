c362b154043c8a70914066dc2a6f018ca9b74c6e
marc monjon <sysmarc13@hotmail.com>
2019-02-05 20:01:50 +0100

    Refactorizado con casos test finalizado

--- Tiendas/src/main/java/org/lasencinas/Porb/Ventas.java
+++ Tiendas/src/main/java/org/lasencinas/Porb/Ventas.java
@@ -1,63 +1,113 @@
 package org.lasencinas.Porb;
 
 public class Ventas {
-    double mayor = 0;
-    double menor = 0;
-    double total = 0;
-    int posicionMayor = 0;
-    int posicionMenor = 0;
+
+    // ------------------Iniciación de Variables--------------------//
+
+    double tiendaMayorIngresos = 0;
+    double tiendaMenorIngresos = 0;
+    double totalTiendasGanancias = 0;
+    int idTiendaMayorIngresos = 0;
+    int idTiendaMenorIngresos = 0;
+    String nombreComercio = "";
     private double[] ventas;
 
-    public Ventas(double[] ventas) {
+    // ----------------Fin iniciación -------------------------//
+
+    // ---------------------Iniciación de Constructor------------------------//
+    public Ventas(double[] ventas, String nombreComercio) {
 	this.ventas = ventas;
+	this.nombreComercio = nombreComercio;
+	tiendaMayorIngresos = getVentas()[0];
+	tiendaMenorIngresos = getVentas()[0];
     }
 
+    // ------------------------Getters y Setters-------------------------------//
     public int getCantidadTiendas() {
-	return this.ventas.length;
+	return getVentas().length;
+    }
+    public String getNombreComercio() {
+	return this.nombreComercio;
+   }
+
+    public double[] getVentas() {
+	return this.ventas;
     }
 
     public double getTiendaMasIngresos() {
-	return this.mayor;
+	return this.tiendaMayorIngresos;
     }
 
     public double getTiendaMenosIngresos() {
-	return this.menor;
+	return this.tiendaMenorIngresos;
     }
 
     public int getTiendaIdMenosVentas() {
-	return posicionMenor + 1;
+	return idTiendaMenorIngresos + 1;
     }
 
     public int getTiendaIdMasVentas() {
-	return posicionMayor + 1;
+	return idTiendaMayorIngresos + 1;
     }
 
-    public double getTotalTiendasVentas() {
-	return this.total;
+    public double getTotalGananciasTiendas() {
+	return this.totalTiendasGanancias;
     }
+
+    public void setMenorIngresosTienda(double venta) {
+	this.tiendaMenorIngresos = venta;
+    }
+
+    public void setMayorIngresosTienda(double venta) {
+	this.tiendaMayorIngresos = venta;
+    }
+
+    public void setTiendaIdMenosVentas(int id) {
+	idTiendaMenorIngresos = id;
+    }
+
+    public void setTiendaIdMasVentas(int id) {
+	idTiendaMenorIngresos = id;
+    }
+
+    public void setTiendasGananciaTotal(double ganancias) {
+	totalTiendasGanancias += ganancias;
+    }
+
+    // ----------------Fin de Getters y Setters--------------------------//
+
+    // ----------------Inicio de métodos de lógica-------------------------//
 
     public void calcularVentas() {
-	mayor = ventas[0];
-	menor = ventas[0];
-	for (int i = 0; i < ventas.length; i++) {
-	    if (ventas[i] > mayor) {
-		mayor = ventas[i];
-		posicionMayor = i;
-	    } else if (ventas[i] < menor) {
-		menor = ventas[i];
-		posicionMenor = i;
+
+	for (int tienda = 0; tienda < getCantidadTiendas(); tienda++) {
+	    if (getVentas()[tienda] > getTiendaMasIngresos()) {
+		setMayorIngresosTienda(getVentas()[tienda]);
+		setTiendaIdMasVentas(tienda);
+	    } else if (getVentas()[tienda] < getTiendaMenosIngresos()) {
+		setMenorIngresosTienda(getVentas()[tienda]);
+		setTiendaIdMenosVentas(tienda);
 	    }
-	    total += ventas[i];
+	    setTiendasGananciaTotal(getVentas()[tienda]);
 
 	}
 
     }
+    // ----------------Fin de métodos de lógica-------------------------//
 
-    public void mostrarInformacion() {
-	System.out.println("La tienda que menos ha vendido fue la " + (posicionMenor + 1)
-		+ ",                  que vendió " + menor + " €");
-	System.out.println("La tienda que mas ha vendido fue la " + (posicionMayor + 1)
-		+ ", que                 vendió " + mayor + " €");
-	System.out.println("El total de las ventas es: " + total + " €");
+    public void mostrarTiendaMenosIngresos() {
+	System.out.println("La tienda que menos ha vendido de " + getNombreComercio() + " fue la " + (idTiendaMenorIngresos + 1) + ",que vendió "
+		+ tiendaMenorIngresos + " €");
+
+    }
+
+    public void mostrarTiendaMasIngresos() {
+	System.out.println("La tienda que más ha vendido de  " + getNombreComercio()+ " fue la " + (idTiendaMayorIngresos + 1) + ", que  vendió "
+		+ tiendaMayorIngresos + " €");
+    }
+
+    public void mostrarTotalGananciasTiendas() {
+	System.out.println("El total de las ventas de " + getNombreComercio() + " es: " + totalTiendasGanancias + " €");
+
     }
 }
