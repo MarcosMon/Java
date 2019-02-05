910328edddbce71c4872c6abc984b1600231c810
marc monjon <sysmarc13@hotmail.com>
2019-02-05 20:03:30 +0100

    Casos test finalizado

--- Tiendas/src/test/java/org/lasencinas/Porb/VentasTest.java
+++ Tiendas/src/test/java/org/lasencinas/Porb/VentasTest.java
@@ -4,33 +4,85 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-/**
- * 
- * Unit test for simple App.
- */
-public class VentasTest 
-{
-    
-    
-    
-    @Test 
-    public void testTiendas() {
+public class VentasTest {
+
+    @Test
+    public void testTiendasPortoPi() {
+	// Total de ventas del comercio //
 	double[] ventas = { 8000, 320.35, 930, 1020.99, 650 };
-	Ventas tiendas = new Ventas(ventas);
-	tiendas.calcularVentas();
-	tiendas.mostrarInformacion();
-	assertEquals(5, tiendas.getCantidadTiendas());
+	// Iniciación de supuestos a testear //
+	int totalTiendas = 5;
+	String nombreComercio = "PortoPi";
+	double cantidadTotal = 10921.34;
+	double menorIngreso = 320.35;
+	double mayorIngreso = 8000;
+	int tiendaID1 = 1;
+	int tiendaID2 = 2;
+	Ventas portoPi = new Ventas(ventas, nombreComercio);
 	
-	assertEquals(8000,tiendas.getTiendaMasIngresos(), 0.001);
+	// Llamada al método calcular Ventas //
+	portoPi.calcularVentas();
 	
-	assertEquals(320.35, tiendas.getTiendaMenosIngresos(), 0.001);
+	// Obtener el total de nuestras tiendas PortoPi //
+	assertEquals(totalTiendas, portoPi.getCantidadTiendas());
+
+	// Obtener el mayor ingreso de nuestras tiendas //
+	assertEquals(mayorIngreso, portoPi.getTiendaMasIngresos(), 0.001);
+
+	// Obtener el menor ingreso de nuestras tiendas //
+	assertEquals(menorIngreso, portoPi.getTiendaMenosIngresos(), 0.001);
 	
-	assertEquals(10921.34, tiendas.getTotalTiendasVentas(), 0.001);
+	// Obtener el total de ingresos de nuestras tiendas
+	assertEquals(cantidadTotal, portoPi.getTotalGananciasTiendas(), 0.001);
 	
-	assertEquals(1, tiendas.getTiendaIdMasVentas());
-	assertEquals(2, tiendas.getTiendaIdMenosVentas());
+	// Obtener el ID de la tienda con más Ventas //
+	assertEquals(tiendaID1, portoPi.getTiendaIdMasVentas());
+	
+	// Obtener el ID de la tienda con menos Ventas //
+	assertEquals(tiendaID2, portoPi.getTiendaIdMenosVentas());
+	
+	// Mostrar la información de la tienda con menos Ventas //
+	portoPi.mostrarTiendaMenosIngresos();
+
+    }
     
-}
-}
+    @Test
+    public void testTiendasFestivalPark() {
+	// Total de ventas del comercio //
+	double[] ventas = { 5000, 720.35, 230, 999.99, 540, 100, 950 };
+	// Iniciación de supuestos a testear //
+	int totalTiendas = 7;
+	String nombreComercio = "FestivalPark";
+	double cantidadTotal = 8540.34;
+	double menorIngreso = 100;
+	double mayorIngreso = 5000;
+	int tiendaID1 = 1;
+	int tiendaID2 = 6;
+	Ventas festivalPark = new Ventas(ventas, nombreComercio);
+	// Llamada al método calcular Ventas //
+	
+	festivalPark.calcularVentas();
+	
+	// Obtener el total de nuestras tiendas FestivalPark //
+	assertEquals(totalTiendas, festivalPark.getCantidadTiendas());
 
+	// Obtener el mayor ingreso de nuestras tiendas //
+	assertEquals(mayorIngreso, festivalPark.getTiendaMasIngresos(), 0.001);
 
+	// Obtener el menor ingreso de nuestras tiendas //
+	assertEquals(menorIngreso, festivalPark.getTiendaMenosIngresos(), 0.001);
+	
+	// Obtener el total de ingresos de nuestras tiendas
+	assertEquals(cantidadTotal, festivalPark.getTotalGananciasTiendas(), 0.001);
+	
+	// Obtener el ID de la tienda con más Ventas //
+	assertEquals(tiendaID1, festivalPark.getTiendaIdMasVentas());
+	
+	// Obtener el ID de la tienda con menos Ventas //
+	assertEquals(tiendaID2, festivalPark.getTiendaIdMenosVentas());
+	
+	// Mostrar la información de la tienda con menos Ventas //
+	festivalPark.mostrarTiendaMenosIngresos();
+
+    }
+}
