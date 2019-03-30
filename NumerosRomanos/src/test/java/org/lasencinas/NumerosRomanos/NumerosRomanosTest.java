588557407c31924b43ef93b2d7e960e86315e0f9
marc monjon <sysmarc13@hotmail.com>
2019-03-30 17:09:40 +0100

    Añadidos mas casos test de numeros no validos

--- NumerosRomanos/src/test/java/org/lasencinas/NumerosRomanos/NumerosRomanosTest.java
+++ NumerosRomanos/src/test/java/org/lasencinas/NumerosRomanos/NumerosRomanosTest.java
@@ -30,38 +30,38 @@ public class NumerosRomanosTest {
 
     @Test
     public void comprobacionNumeroValido() {
-	
-	//----------Primer Caso-----------//
+
+	// ----------Primer Caso-----------//
 	String numValido = "XXX";
 	numeroRomano = new NumerosRomanos(numValido);
 	assertTrue("El número es válido", numeroRomano.comprobarNumero());
 
-	//----------Segundo Caso-----------//
+	// ----------Segundo Caso-----------//
 	String numValidoDos = "XL";
 	numeroRomano = new NumerosRomanos(numValidoDos);
 	assertTrue("El número es válido", numeroRomano.comprobarNumero());
 
-	//----------Tercer Caso-----------//
+	// ----------Tercer Caso-----------//
 	String numValidoTres = "CM";
 	numeroRomano = new NumerosRomanos(numValidoTres);
 	assertTrue("El número es válido", numeroRomano.comprobarNumero());
 
-	//----------Cuarto Caso-----------//
+	// ----------Cuarto Caso-----------//
 	String numValidoCuatro = "MMMDCCCLXXXVIII";
 	numeroRomano = new NumerosRomanos(numValidoCuatro);
 	assertTrue("El número es válido", numeroRomano.comprobarNumero());
-	
-	//----------Quinto Caso-----------//
+
+	// ----------Quinto Caso-----------//
 	String numValidoCinco = "MMDCCLXXVII";
 	numeroRomano = new NumerosRomanos(numValidoCinco);
 	assertTrue("El número es válido", numeroRomano.comprobarNumero());
-	
-	//----------Sexto Caso-----------//
+
+	// ----------Sexto Caso-----------//
 	String numValidoSeis = "CDXLIV";
 	numeroRomano = new NumerosRomanos(numValidoSeis);
 	assertTrue("El número es válido", numeroRomano.comprobarNumero());
-	
-	//----------Séptimo Caso-----------//
+
+	// ----------Séptimo Caso-----------//
 	String numValidoSiete = "CDXXXIX";
 	numeroRomano = new NumerosRomanos(numValidoSiete);
 	assertTrue("El número es válido", numeroRomano.comprobarNumero());
@@ -70,8 +70,39 @@ public class NumerosRomanosTest {
     @Test
     public void comprobacionNumeroNoValido() {
 
+	// ----------Primer Caso-----------//
 	String numValido = "XXXX";
 	numeroRomano = new NumerosRomanos(numValido);
+	assertFalse("El número no es válido", numeroRomano.comprobarNumero());
+
+	// ----------Segundo Caso-----------//
+	String numValidoDos = "IVIV";
+	numeroRomano = new NumerosRomanos(numValidoDos);
+	assertFalse("El número no es válido", numeroRomano.comprobarNumero());
+
+	// ----------Tercer Caso-----------//
+	String numValidoTres = "IVCM";
+	numeroRomano = new NumerosRomanos(numValidoTres);
+	assertFalse("El número no es válido", numeroRomano.comprobarNumero());
+
+	// ----------Cuarto Caso-----------//
+	String numValidoCuatro = "XIIII";
+	numeroRomano = new NumerosRomanos(numValidoCuatro);
+	assertFalse("El número no es válido", numeroRomano.comprobarNumero());
+
+	// ----------Quinto Caso-----------//
+	String numValidoCinco = "CCMMDX";
+	numeroRomano = new NumerosRomanos(numValidoCinco);
+	assertFalse("El número no es válido", numeroRomano.comprobarNumero());
+
+	// ----------Sexto Caso-----------//
+	String numValidoSeis = "MMMLL";
+	numeroRomano = new NumerosRomanos(numValidoSeis);
+	assertFalse("El número no es válido", numeroRomano.comprobarNumero());
+
+	// ----------Séptimo Caso-----------//
+	String numValidoSiete = "CLMDVXI";
+	numeroRomano = new NumerosRomanos(numValidoSiete);
 	assertFalse("El número no es válido", numeroRomano.comprobarNumero());
 
     }
